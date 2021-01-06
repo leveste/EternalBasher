45f6ffa9279cc0946608ef6fe2f525990fd3be84
PowerBall253 <77066815+PowerBall253@users.noreply.github.com>
2021-01-06 15:42:57 -0600

    Update EternalModInjector.sh

--- EternalModInjector.sh
+++ EternalModInjector.sh
@@ -27,5 +27,5 @@ export ___HAS_CHECKED_RESOURCES=""
 export ___HAS_READ_FIRST_TIME=""
 export ___RESET_BACKUPS=""
 
-if [ -f base/game/sp/e1m1_intro/e1m1_intro.resources ]; then export $___OWNS_CAMPAIGN=true;     fi
-if [ -f base/game/dlc/e4m1_rig/e4m1_rig.resources ]; then export $___OWNS_ANCIENT_GODS_ONE=true;     fi
+if [ -f base/game/sp/e1m1_intro/e1m1_intro.resources ]; then export ___OWNS_CAMPAIGN=true;     fi
+if [ -f base/game/dlc/e4m1_rig/e4m1_rig.resources ]; then export ___OWNS_ANCIENT_GODS_ONE=true;     fi
