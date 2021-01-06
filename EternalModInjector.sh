3f6c012ff48ef0645e3fcffc502ec03145c2b1f9
Loghin Vlad <stefan9vlad@yahoo.com>
2021-01-06 22:12:44 +0200

    fixed extra export calls

--- EternalModInjector.sh
+++ EternalModInjector.sh
@@ -6,7 +6,7 @@ printf "Based on original batch file by Zwip-Zwap Zapony\n\n\n"
 export ___CONFIGURATION_FILE="EternalModInjector Settings.txt"
 export ___CONFIGURATION_FILE_OLD="EternalModInjector.dat"
 export ___GAME_EXE="DOOMEternalx64vk.exe"
-export 
+
 export ___ASSET_VERSION=4.1
 export ___DETERNAL_LOADMODS_MD5="43c54928c12d5d72c32f563f01dc7aef"
 export ___ETERNALPATCHER_MD5="8033260ff14c2ee441b81bbf8d3b2de0"
@@ -14,12 +14,12 @@ export ___IDREHASH_MD5="50747578b8e29c3da1aa5a3ac5d28cc7"
 export ___PATCHED_GAME_MD5="3238e7a9277efc6a607b1b1615ebe79f 4acdaf89f30f178ba9594c0364b35a30"
 export ___VANILLA_GAME_MD5="1ef861b693cdaa45eba891d084e5f3a3 c2b429b2eb398f836dd10d22944b9c76"
 export ___VANILLA_META_MD5="4f4deb1df8761dc8fd2d3b25a12d8d91"
-export 
+
 export ___CERTUTIL_EXISTS=1
 export ___GAME_HAS_BEEN_PATCHED=""
 export ___OWNS_ANCIENT_GODS_ONE=""
 export ___OWNS_CAMPAIGN=""
-export 
+ 
 export ___CONFIGURATION_EXISTS=""
 export ___AUTO_LAUNCH_GAME=1
 export ___GAME_PARAMETERS=""
