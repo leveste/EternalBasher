e96c740f1125a0bff51b3696360514e86d971489
PowerBall253 <77066815+PowerBall253@users.noreply.github.com>
2021-01-09 14:32:23 -0600

    Update EternalModInjector_New.sh

--- EternalModInjector/EternalModInjector_New.sh
+++ EternalModInjector/EternalModInjector_New.sh
@@ -16,7 +16,8 @@ ASSET_VERSION=4.1
 DETERNAL_LOADMODS_MD5="43c54928c12d5d72c32f563f01dc7aef"
 ETERNALPATCHER_MD5="8033260ff14c2ee441b81bbf8d3b2de0"
 IDREHASH_MD5="50747578b8e29c3da1aa5a3ac5d28cc7"
-PATCHED_GAME_MD5="3238e7a9277efc6a607b1b1615ebe79f 4acdaf89f30f178ba9594c0364b35a30"
+PATCHED_GAME_MD5_A="3238e7a9277efc6a607b1b1615ebe79f"
+PATCHED_GAME_MD5_B="4acdaf89f30f178ba9594c0364b35a30"
 VANILLA_GAME_MD5="1ef861b693cdaa45eba891d084e5f3a3 c2b429b2eb398f836dd10d22944b9c76"
 VANILLA_META_MD5="4f4deb1df8761dc8fd2d3b25a12d8d91"
 
@@ -28,7 +29,7 @@ if ! [ $IDREHASH_MD5 == $idRehashMD5 ]; then MissingDEternalLoadMods; fi
 
 #Patch Game Executable
 GameMD5=($(md5sum DOOMEternalx64vk.exe))
-if ! [[ $VANILLA_GAME_MD5 == $GameMD5 ]] || [[ $PATCHED_GAME_MD5 == $GameMD5 ]] ; then CorruptedGameExecutable; fi
+if ! [[ $VANILLA_GAME_MD5 == $GameMD5 ]] || [[ $PATCHED_GAME_MD5_A == $GameMD5 ]] || [[ $PATCHED_GAME_MD5_B == $GameMD5 ]]; then CorruptedGameExecutable; fi
 
 if [[ $VANILLA_GAME_MD5 == $GameMD5 ]]
 then
