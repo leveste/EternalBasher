3520fe49238ec5b46eebf7447cefc49ad41628e4
PowerBall253 <77066815+PowerBall253@users.noreply.github.com>
2021-01-10 10:50:35 -0600

    Update EternalModInjector_New.sh

--- EternalModInjector/EternalModInjector_New.sh
+++ EternalModInjector/EternalModInjector_New.sh
@@ -359,8 +359,8 @@ while IFS= read -r filename; do
 	Backed up $name
 	"; fi
 	filename=${name%.resources}
-	grep -v "${filename}.backup" "EternalModInjector Settings.txt" > nobackups.txt; mv nobackups.txt "EternalModInjector Settings.txt"
-	( if ! grep -q "${filename}.backup" "$CONFIG_FILE"; then echo ${filename}.backup >> "EternalModInjector Settings.txt"; fi ) > /dev/null 2>&1
+	grep -v -q "${filename}.backup" "EternalModInjector Settings.txt" > nobackups.txt; mv nobackups.txt "EternalModInjector Settings.txt"
+	if ! grep -q "${filename}.backup" "$CONFIG_FILE"; then echo ${filename}.backup >> "EternalModInjector Settings.txt"; fi
 	echo ${filename}.resources >> "EternalModInjector Settings.txt"
 done < modloaderlist.txt
 rm modloaderlist.txt
