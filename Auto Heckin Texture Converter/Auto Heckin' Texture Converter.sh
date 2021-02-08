e1bb5d677cba6b6ab4fecf23ccd7cc6abbab7ef3
Loghin Vlad <stefan9vlad@yahoo.com>
2021-02-08 18:40:10 +0200

    finished script; pending testing

--- Auto Heckin Texture Converter/Auto Heckin' Texture Converter.sh
+++ Auto Heckin Texture Converter/Auto Heckin' Texture Converter.sh
@@ -18,6 +18,28 @@ if [ "$#" -eq 0 ]
 then
 	printf "\n\nNo arguments found. Please pass the files you wish to convert as arguments, as shown below.\n
 	./\"Auto Heckin' Texture Converter.sh\" [texture1] [texture2] [...]\n\n"
+
+	exit
 fi
 
+while [ $# -ne 0 ]
+do
+	echo "Converting '$1'..."
 
+	cd tools
+	wine nvcompress.exe -bcla -fast "$1" "${1}.dds" > /dev/null
+	cd ..
+	wine ./tools/DivinityMashine.exe "${1}.dds" > /dev/null
+
+	# remove file extensions
+	filename="${$i}"
+	filename="${filename%%.*}" > /dev/null
+
+	name="${1}.dds"
+	tga_name="${name//dds/tga}"
+
+	mv "$tga_name" "${filename}.tga" > /dev/null
+	rm "${1}.dds" > /dev/null
+
+	shift
+done
