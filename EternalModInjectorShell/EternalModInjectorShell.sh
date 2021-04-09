21a24ebd13934221ad7828e73e33563c5a7db7f2
PowerBall253 <brunoanconasala@gmail.com>
2021-04-09 15:39:46 -0500

    Change license to GPL 3.0

--- EternalModInjectorShell/EternalModInjectorShell.sh
+++ EternalModInjectorShell/EternalModInjectorShell.sh
@@ -1,3 +1,19 @@
+# This file is part of EternalBasher (https://github.com/leveste/EternalBasher).
+# Copyright (C) 2021 leveste and PowerBall253
+#
+# EternalBasher is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# EternalBasher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of  
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
+# GNU General Public License for more details.  
+#
+# You should have received a copy of the GNU General Public License  
+# along with EternalBasher. If not, see <https://www.gnu.org/licenses/>.
+
 #!/usr/bin/env bash
 
 #Script version
