5da51b11f76d49e3a33de2f8a416057937ecff91
PowerBall253 <brunoanconasala@gmail.com>
2022-05-05 21:43:40 -0500

    Bump version number

--- EternalModInjectorShell/EternalModInjectorShell.sh
+++ EternalModInjectorShell/EternalModInjectorShell.sh
@@ -17,7 +17,7 @@
 # along with EternalBasher. If not, see <https://www.gnu.org/licenses/>.
 
 # Script version
-script_version="v6.66-rev2.1"
+script_version="v6.66-rev2.2"
 
 # Game version
 game_version="6.66 Rev 2"
