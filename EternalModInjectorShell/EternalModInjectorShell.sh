1e967d4c36db649b874cac63ca28af9f7e273dfa
Bruno Ancona <brunoanconasala@gmail.com>
2024-08-18 10:09:21 -0600

    Bump version number

--- EternalModInjectorShell/EternalModInjectorShell.sh
+++ EternalModInjectorShell/EternalModInjectorShell.sh
@@ -17,7 +17,7 @@
 # along with EternalBasher. If not, see <https://www.gnu.org/licenses/>.
 
 # Script version
-script_version="v6.66-rev2.2.4"
+script_version="v6.66-rev3.0"
 
 # Game version
 game_version="6.66 Rev 3"
